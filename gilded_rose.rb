ea2de5b00611e38f714574b708d314c31a01348c
mostafa <mostafanabil198@gmail.com>
2019-07-27 07:13:43 +0200

    solution

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -3,50 +3,31 @@ class GildedRose
     @items = items
   end
 
-  def update_quality()
+  def update_quality
     @items.each do |item|
-      if item.name != 'Aged Brie' and item.name != 'Backstage passes to a TAFKAL80ETC concert'
-        if item.quality > 0
-          if item.name != 'Sulfuras, Hand of Ragnaros'
-            item.quality = item.quality - 1
-          end
+      if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert' && item.name != 'Sulfuras' && item.name != 'Hand of Ragnaros'
+        if item.sell_in > 0
+          x = 1
+          x = 2 if item.name == 'Conjured'
+          item.sell_in = item.sell_in - 1 if item.sell_in > 0
+          item.quality = item.quality / 2 * x if item.sell_in == 0
+          item.quality = item.quality - 1 * x if item.quality > 0 && item.sell_in != 0
         end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != 'Sulfuras, Hand of Ragnaros'
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != 'Aged Brie'
-          if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-            if item.quality > 0
-              if item.name != 'Sulfuras, Hand of Ragnaros'
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
+      elsif item.name == 'Aged Brie' || item.name == 'Backstage passes'
+        if item.sell_in > 0
+          item.sell_in = item.sell_in - 1
+          item.quality = 0 if item.sell_in == 0
+          if item.sell_in <= 5 && item.quality <= 47
+            item.quality = item.quality + 3
+          elsif item.sell_in <= 10 && item.quality <= 48
+            item.quality = item.quality + 2
+          elsif item.sell_in > 10 && item.quality <= 49
             item.quality = item.quality + 1
           end
         end
+        item.quality = 0 if item.sell_in == 0
+      elsif item.name == 'Conjured'
+
       end
     end
   end
@@ -58,7 +39,11 @@ class Item
   def initialize(name, sell_in, quality)
     @name = name
     @sell_in = sell_in
-    @quality = quality
+    if quality >= 0
+      @quality = quality
+    else
+      raise ArgumentError.new("Quality cant be negative")
+    end
   end
 
   def to_s()
--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -1,11 +1,113 @@
+require 'rspec/autorun'
 require File.join(File.dirname(__FILE__), 'gilded_rose')
 
-describe GildedRose do
-  describe '#update_quality' do
+RSpec.describe GildedRose do
+  describe 'reduce item variables' do
+    # let(:item) { [Item.new('foo', 5, 10)] }
     it 'does not change the name' do
-      items = [Item.new('foo', 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq 'fixme'
+      item = [Item.new('foo', 5, 10)]
+      GildedRose.new(item).update_quality()
+      expect(item[0].name).to eq('foo')
+    end
+
+    it 'reduce sell_in' do
+      item = [Item.new('foo', 5, 10)]
+      GildedRose.new(item).update_quality()
+      expect(item[0].sell_in).to eq(4)
+    end
+
+    it 'reduce quality' do
+      item = [Item.new('foo', 5, 10)]
+      GildedRose.new(item).update_quality()
+      expect(item[0].quality).to eq(9)
+    end
+
+    it 'reduce sell_in for Conjured' do
+      item = [Item.new('Conjured', 5, 10)]
+      GildedRose.new(item).update_quality()
+      expect(item[0].sell_in).to eq(4)
+    end
+
+    it 'reduce quality for Conjured' do
+      item = [Item.new('Conjured', 5, 10)]
+      GildedRose.new(item).update_quality()
+      expect(item[0].quality).to eq(8)
+    end
+
+  end
+
+  describe 'features' do
+    it 'Once the sell by date has passed, Quality degrades twice as fast' do
+      item = [Item.new('foo', 1, 10)]
+      GildedRose.new(item).update_quality()
+      expect(item[0].quality).to eq(5)
+    end
+
+    it 'The Quality of an item is never negative 1' do
+      expect{(Item.new('foo', 0, -10)).to raise_error(Errors::ArgumentError)}
+    end
+
+    it 'The Quality of an item is never negative 2' do
+      item = [Item.new('foo', 1, 0)]
+      GildedRose.new(item).update_quality()
+      expect(item[0].quality).to eq(0)
+    end
+
+    describe 'Aged Brie' do
+      it 'Aged Brie actually increases in Quality' do
+        item = [Item.new('Aged Brie', 15, 15)]
+        GildedRose.new(item).update_quality()
+        expect(item[0].quality).to eq(16)
+      end
+      it 'sellin less than 5' do
+        item = [Item.new('Aged Brie', 2, 10)]
+        GildedRose.new(item).update_quality()
+        expect(item[0].quality).to eq(13)
+      end
+      it 'sellin equal than 5' do
+        item = [Item.new('Aged Brie', 5, 10)]
+        GildedRose.new(item).update_quality()
+        expect(item[0].quality).to eq(13)
+      end
+      it 'sellin less than 10' do
+        item = [Item.new('Aged Brie', 8, 10)]
+        GildedRose.new(item).update_quality()
+        expect(item[0].quality).to eq(12)
+      end
+      it 'sellin equal than 10' do
+        item = [Item.new('Aged Brie', 10, 10)]
+        GildedRose.new(item).update_quality()
+        expect(item[0].quality).to eq(12)
+      end
+      it 'drops to zero' do
+        item = [Item.new('Aged Brie', 1, 10)]
+        GildedRose.new(item).update_quality()
+        expect(item[0].quality).to eq(0)
+      end
+      it 'drops to zero' do
+        item = [Item.new('Aged Brie', 0, 10)]
+        GildedRose.new(item).update_quality()
+        expect(item[0].quality).to eq(0)
+      end
+    end
+
+    it 'The Quality of an item is never more than 50' do
+      item = [Item.new('Aged Brie', 10, 50)]
+      GildedRose.new(item).update_quality()
+      expect(item[0].quality).to eq(50)
+    end
+
+    describe 'Sulfuras, being a legendary item' do
+      it 'never has to be sold' do
+        item = [Item.new('Sulfuras', 10, 10)]
+        GildedRose.new(item).update_quality()
+        expect(item[0].sell_in).to eq(10)
+      end
+      it 'never has to decrease quality' do
+        item = [Item.new('Sulfuras', 10, 10)]
+        GildedRose.new(item).update_quality()
+        expect(item[0].quality).to eq(10)
+      end
     end
   end
 end
